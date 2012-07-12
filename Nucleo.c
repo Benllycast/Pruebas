61e2ff9a78284c3fd6045e77ad530d02bf95f305
Benllycast <benllycast@gmail.com>
2012-07-12 11:06:04 -0500

    12 julio 2012

--- Nucleo.c
+++ Nucleo.c
@@ -1,35 +1,24 @@
-#include <18F67J50.h>
 #include "Nucleo.h"
-#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#INT_SSP
-void  SSP_isr(VOID) {
-	  
-}            
 
-void main(){
-   setup_adc_ports ( ALL_ANALOG|VSS_VDD ) ;
-   setup_adc ( ADC_CLOCK_DIV_2|ADC_TAD_MUL_0 ) ;
-   //setup_psp ( PSP_DISABLED ) ;
-   setup_spi ( SPI_MASTER|SPI_L_TO_H|SPI_CLK_DIV_4 ) ;
-   setup_spi2 ( SPI_SS_DISABLED ) ;
-   setup_wdt ( WDT_OFF ) ;
-   setup_timer_0 ( RTCC_INTERNAL ) ;
-   setup_timer_1 ( T1_DISABLED ) ;
-   setup_timer_2 ( T2_DISABLED, 0, 1 ) ;
-   setup_timer_3 ( T3_DISABLED|T3_DIV_BY_1 ) ;
-   setup_timer_4 ( T4_DISABLED, 0, 1 ) ;
-   setup_ccp1 ( CCP_OFF ) ;
-   setup_comparator ( NC_NC_NC_NC ); // This device COMP currently not supported by the PICWizard
-   enable_interrupts ( INT_SSP ) ;
-   enable_interrupts ( GLOBAL ) ;
+void main()
+{
 
-   //Setup_Oscillator parameter not selected from Intr Oscillator Config tab
+   setup_adc_ports(NO_ANALOGS|VSS_VDD);
+   setup_adc(ADC_CLOCK_DIV_2|ADC_TAD_MUL_0);
+   setup_psp(PSP_DISABLED);
+  setup_spi(SPI_MASTER|SPI_L_TO_H|SPI_CLK_DIV_4);
+   setup_wdt(WDT_OFF);
+   setup_timer_0(RTCC_INTERNAL);
+   setup_timer_1(T1_DISABLED);
+   setup_timer_2(T2_DISABLED,0,1);
+   setup_timer_3(T3_DISABLED|T3_DIV_BY_1);
+   setup_ccp1(CCP_OFF);
+   setup_comparator(NC_NC_NC_NC);
+   setup_vref(FALSE);
+//Setup_Oscillator parameter not selected from Intr Oscillator Config tab
+
    // TODO: USER CODE!!
-
-   while(true){
-      true;
-   }
 }
-
--- Nucleo.h
+++ Nucleo.h
@@ -1,18 +1,30 @@
-
+#include <18F4620.h>
 #device adc=8
 
-#FUSES NOWDT                    //No Watch Dog Timer
-#FUSES WDT128                   //Watch Dog Timer uses 1:128 Postscale
-#FUSES HS                       //High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
-#FUSES NODEBUG                  //No Debug mode for ICD
-#FUSES NOXINST                  //Extended set extension and Indexed Addressing mode disabled (Legacy mode)
-#FUSES STVREN                   //Stack full/underflow will cause reset
-#FUSES NOPROTECT                //Code not protected from reading
-#FUSES FCMEN                    //Fail-safe clock monitor enabled
-#FUSES IESO                     //Internal External Switch Over mode enabled
-#FUSES MSSPMSK7              
-#FUSES NOCPUDIV              
-#FUSES PLL1                     //No PLL PreScaler
+#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES WDT128                	//Watch Dog Timer uses 1:128 Postscale
+#FUSES HS                    	//High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
+#FUSES NOPROTECT             	//Code not protected from reading
+#FUSES IESO                  	//Internal External Switch Over mode enabled
+#FUSES NOBROWNOUT            	//No brownout reset
+#FUSES BORV21                	//Brownout reset at 2.1V
+#FUSES NOPUT                 	//No Power Up Timer
+#FUSES NOCPD                 	//No EE protection
+#FUSES STVREN                	//Stack full/underflow will cause reset
+#FUSES NODEBUG               	//No Debug mode for ICD
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOWRT                 	//Program memory not write protected
+#FUSES NOWRTD                	//Data EEPROM not write protected
+#FUSES NOEBTR                	//Memory not protected from table reads
+#FUSES NOCPB                 	//No Boot Block code protection
+#FUSES NOEBTRB               	//Boot block not protected from table reads
+#FUSES NOWRTC                	//configuration not registers write protected
+#FUSES NOWRTB                	//Boot block not write protected
+#FUSES FCMEN                 	//Fail-safe clock monitor enabled
+#FUSES NOXINST               	//Extended set extension and Indexed Addressing mode disabled (Legacy mode)
+#FUSES PBADEN                	//PORTB pins are configured as analog input channels on RESET
+#FUSES LPT1OSC               	//Timer1 configured for low-power operation
+#FUSES MCLR                  	//Master Clear pin enabled
 
-#use delay(clock=20000000)
+#use delay(clock=10000000,RESTART_WDT)
 
