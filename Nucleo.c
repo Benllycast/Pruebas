1f3f32dea700f4d5f1f18c82abb46554268d5a14
Benllycast <benllycast@gmail.com>
2012-08-21 19:43:54 -0500

    martes 21 de agosto 2012

--- Nucleo.c
+++ Nucleo.c
@@ -6,7 +6,7 @@
 #include "captura_frecuencia.h"
 
 extern CONFIG_MMA7455 CONFIG;
-//extern unsigned int32 tiempo_inicial, tiempo_final;
+extern unsigned int32 tiempo_inicial, tiempo_final;
 void setup_devices(){
    int error = -1;
    /////////////configuracion del MMA7455////////////////
@@ -15,7 +15,6 @@ void setup_devices(){
    //error = AD_init_adc();
    //////////////////////////////////////////////////////
    error = CP_init_ccp();
-   printf("init_error = %d\n\r", error);
    setup_psp(PSP_DISABLED);
    setup_wdt(WDT_OFF);
    setup_timer_0(RTCC_INTERNAL);
@@ -23,7 +22,7 @@ void setup_devices(){
    setup_timer_2(T2_DISABLED,0,1);
    //setup_timer_3(T3_DISABLED|T3_DIV_BY_1);
    //setup_ccp1(CCP_OFF);
-   //setup_comparator(NC_NC_NC_NC);
+   setup_comparator(NC_NC_NC_NC);
    setup_vref(FALSE);
    return;
 }
@@ -36,8 +35,8 @@ void main()
    // TODO: USER CODE!!  
    while(error == 0){
       error = CP_leer_ccp(CANAL_2, &value);
-      printf("CCP canal 2: %Lu \n\r", value);
-      delay_ms(250);
+      printf("%Lu,%Lu,%Lu\n\r", tiempo_inicial,tiempo_final, value);
+      //delay_ms(250);
    }
 }
 
