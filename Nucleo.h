6e36fdbb1d55eda89cc0ce0073fcaa5293dab981
Benllycast <benllycast@gmail.com>
2014-03-08 14:13:44 -0500

    actualizacion del circuito y fuses de configuracion

--- Nucleo.h
+++ Nucleo.h
@@ -14,7 +14,7 @@ frecuencia de la CPU:24 Mhz; el FUSE CPIDIV3 para dividir los 96Mhz hasta 24MHz 
 #define SIMULACION 1	// comentar esto si se prueba en forma real
 
 /*====================fuses de configuracion del dispositivo==================*/
-#fuses HSPLL,NOWDT,NOPROTECT,NOLVP,NODEBUG,USBDIV,PLL5,CPUDIV4,VREGEN, NOPBADEN
+#fuses HSPLL,NOWDT,NOPROTECT,NOLVP,NODEBUG,USBDIV,PLL5,CPUDIV4,VREGEN,NOPBADEN,CCP2C1
 /*
 #FUSES NOWDT                 	//No Watch Dog Timer
 #FUSES WDT128                	//Watch Dog Timer uses 1:128 Postscale
