c6c0121b0f90330e29b3942fab5202b1bee8aa1d
benllycast <benllycast@gmail.com>
2014-04-08 00:48:01 -0500

    agregando nuevas operaciones

--- configuracion.h
+++ configuracion.h
@@ -2,49 +2,52 @@
 #define CONFIGURACION_H
 
 // opciones de configuracion
-#define CONF_SALIR	'9'
-#define CONF_FECHA	'1'
-#define CONF_DATO		'2'
-#define CONF_CANAL	'3'
+#define CONF_SALIR    '9'
+#define CONF_SET_FECHA  '1'
+#define CONF_GET_FECHA  '2'
+#define CONF_SET_CANAL  '3'
+#define CONF_GET_CANAL  '4'
+#define CONF_SET_DATO   '5'
+#define CONF_GET_DATO   '6'
 
-#define CONF_ACK		0x06
-#define CONF_NOACK	0x15
+#define CONF_ACK        0x06
+#define CONF_NOACK    0x15
 
 //direccion del byte de canales activos y bit de canales
-#define CONF_DIR_CANAl		0x08
-#define CONF_CCP_VEL_BIT	0
-#define CONF_CCP_REV_BIT	1
-#define CONF_CCP_BIT			2
-#define CONF_AD_VEL_BIT		3
-#define CONF_AD_REV_BIT		4
+#define CONF_DIR_CANAl        0x08
+#define CONF_CCP_VEL_BIT    0
+#define CONF_CCP_REV_BIT    1
+#define CONF_CCP_BIT            2
+#define CONF_AD_VEL_BIT        3
+#define CONF_AD_REV_BIT        4
 
 // numeracion de sensores
-#define ACC_x		0
-#define ACC_y		1
-#define ACC_z		2
-#define AD_VEL 	3
-#define AD_REV		4
-#define CCP_REV 	5
-#define CCP_VEl 	6
+#define ACC_x        0
+#define ACC_y        1
+#define ACC_z        2
+#define AD_VEL     3
+#define AD_REV        4
+#define CCP_REV     5
+#define CCP_VEl     6
 
 // para el switch de log
-#define LOG_ENABLE	1
-#define LOG_DISABLE	0
+#define LOG_ENABLE    1
+#define LOG_DISABLE    0
 #define LOG_LINE ("\n\r%X:%X:%X:%X:%X:%X:%X:%X:%LX:%LX")
 
 // definicion de pines de indicadores
-#define INDICADOR_AMARILLO	PIN_E0
-#define INDICADOR_USB		PIN_E1
-#define INDICADOR_RUN		PIN_E2
+#define INDICADOR_AMARILLO    PIN_E0
+#define INDICADOR_USB        PIN_E1
+#define INDICADOR_RUN        PIN_E2
 
 // esto es para debug
 #ifdef SIMULACION
-	#define cout	
-	#define cin		getc              
+    #define cout    
+    #define cin        getc              
 #else
-	#define cout	usb_cdc_putc_fast,
-	#define cin		usb_cdc_getc
-	#define putc	usb_cdc_putc
+    #define cout    usb_cdc_putc_fast,
+    #define cin        usb_cdc_getc
+    #define putc    usb_cdc_putc
 #endif
 
 // definciones utilizadas en el nucleo
@@ -52,8 +55,8 @@ int1 salida = 0;
 byte CONF_CANAL_ACTIVO = 0;
 
 #ifdef CAPTURA_FRECUENCIA_H
-	int canal_ccp = CCP_CANAL_1;
-#endif	//CAPTURA_FRECUENCIA_H
+    int canal_ccp = CCP_CANAL_1;
+#endif    //CAPTURA_FRECUENCIA_H
 
 int myerror = 0;
 int16 lectura = 0;
@@ -62,16 +65,16 @@ char testfile[] = "prueba";
 char buffer_log[39];
 //int32 resultado = 0;
 struct Log {
-	byte dia;
-	byte mes;
-	byte anio;
-	byte hor;
-	byte min;
-	byte seg;
-	int8 sensor;
-	int8 no_data;
-	int32 value;
-	int16 crc;
+    byte dia;
+    byte mes;
+    byte anio;
+    byte hor;
+    byte min;
+    byte seg;
+    int8 sensor;
+    int8 no_data;
+    int32 value;
+    int16 crc;
 } data;
 
 void iniciar_perifericos();
